8d95957c2590d182593dc24970750de6c71ab27d
Jeremy Myers <echelonical@gmail.com>
2022-11-26 16:24:04 -0600

    added classes for addition critters and animals

---
+++ animals.py
@@ -0,0 +1,165 @@
+# import the python datetime module to help us create a timestamp
+from datetime import date
+
+class Llama:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = date.today()
+
+class Zebra:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = date.today()
+
+class Giraffe:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = date.today()
+
+class Donkey:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = date.today()
+
+class Gorilla:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.walking = True
+        self.date_added = date.today()
+
+class Goldfish:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.date_added = date.today()
+
+class Mallard:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.date_added = date.today()
+
+class Turtle:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.date_added = date.today()
+
+class Alligator:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.date_added = date.today()
+
+class Shark:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.swimming = True
+        self.date_added = date.today()
+
+class Snail:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.slithering = True
+        self.date_added = date.today()
+
+class Worms:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.slithering = True
+        self.date_added = date.today()
+
+class Salamander:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.slithering = True
+        self.date_added = date.today()
+
+class Snake:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.slithering = True
+        self.date_added = date.today()
+
+class Slug:
+
+    def __init__(self):
+        # Establish the properties of each animal
+        # with a default value
+        self.name = ""
+        self.species = ""
+        self.slithering = True
+        self.date_added = date.today()
+
+
+
+
+
+
+
+
+miss_fuzz =  Llama()
+miss_fuzz.name = "Miss Fuzz"
+miss_fuzz.species = "domestic llama"
+
+print(miss_fuzz)
